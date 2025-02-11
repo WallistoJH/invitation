f254078fc68bba46521471dacb94c2de163bbe9e
jaehan.park <wallisto_jh@icloud.com>
2025-02-11 17:29:51 +0900

    인트로 텍스트 수정

--- js/inv.js
+++ js/inv.js
@@ -1,7 +1,15 @@
 // 스크롤시 메인 블러 시작
 $(document).ready(function () {
+    // 브라우저가 스크롤 위치를 기억하지 않도록 설정
+    // if ("scrollRestoration" in history) {
+    //     history.scrollRestoration = "manual";
+    // }
+    // 페이지 로드 시 맨 위로 이동
+    // $(window).scrollTop(0);
+
+    // 스크롤 이벤트
     $(window).on("scroll", function () {
-        if ($(window).scrollTop() > $(window).height() * 0.2) {
+        if ($(window).scrollTop() > $(window).height() * 0.4) {
             $("#introTitle").addClass("blur");
         } else {
             $("#introTitle").removeClass("blur");
